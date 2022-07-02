30325d2734ddae3d1f28de4d58273abc03372a80
Andrey G <a.gavrilov@promo-bot.ru>
2022-07-02 12:45:25 +0500

    setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,64 +4,64 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String,
   },
   data() {
     return {
       Projects: [
-        { id: 1, name: 'Pipka' },
-        { id: 2, name: 'Bobka' },
-        { id: 3, name: 'Dopka' },
+        { id: 1, name: "Pipka" },
+        { id: 2, name: "Bobka" },
+        { id: 3, name: "Dopka" },
       ],
       users: [
         {
           id: 1,
-          name: 'Vasya',
+          name: "Vasya",
         },
         {
           id: 2,
-          name: 'Petya',
+          name: "Petya",
         },
         {
           id: 3,
-          name: 'Dosya',
+          name: "Dosya",
         },
       ],
       MRs: [
         {
-          title: 'lorem1',
+          title: "lorem1",
           id: 1,
           projectId: 1,
           reviewers: [1, 2, 3],
           liked: [1, 3],
         },
         {
-          title: 'lorem2',
+          title: "lorem2",
           id: 2,
           projectId: 2,
           reviewers: [1, 2, 3],
           liked: [1],
         },
         {
-          title: 'lorem3',
+          title: "lorem3",
           id: 3,
           projectId: 3,
           reviewers: [1, 2, 3],
           liked: [3],
         },
         {
-          title: 'lorem4',
+          title: "lorem4",
           id: 4,
           projectId: 1,
           reviewers: [1, 2, 3],
           liked: [2],
         },
       ],
-    }
+    };
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
