fe2565080fd14e15cee04249e92260e301de237d
Andrey G <a.gavrilov@promo-bot.ru>
2022-07-04 20:43:04 +0500

    Update UserPage.vue

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,11 +1,20 @@
 <template>
   <h1>Страница пользователя с ID = {{ $route.params.id }}</h1>
   <p>Страница пользователя {{ user.name }}</p>
-  <p>{{ likeMr.liked }}</p>
+
+  <p>1 - {{ mr }}</p>
+  <p>2 - {{ mr2 }}</p>
+  <p>3 - {{ mergeRequestId }}</p>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      mergeRequestId: [],
+    }
+  },
+  methods: {},
   //mergeRequests filter includes
   props: {
     users: {
@@ -15,15 +24,26 @@ export default {
       type: Array,
     },
   },
+  mounted() {},
   computed: {
     user() {
       return this.users.find((user) => {
         return user.id === Number(this.$route.params.id)
       })
     },
-    Mr() {
+    mr() {
+      return this.mergeRequests.map((mr) => {
+        return mr.liked.filter((like) => {
+          return like === this.user.id
+        })
+      })
+    },
+
+    mr2() {
       return this.mergeRequests.forEach((mr) => {
-        return mr
+        return this.mergeRequestId.push(mr.liked.filter((like) => {
+          return like === this.user.id
+        }))
       })
     },
   },
