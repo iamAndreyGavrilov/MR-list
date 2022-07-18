845389e23acf33a3a5de6dc1e0e71034c7fc2fa3
Andrey G <a.gavrilov@promo-bot.ru>
2022-07-18 19:35:07 +0500

    end

--- src/components/UserItem.vue
+++ src/components/UserItem.vue
@@ -7,10 +7,51 @@
       <button @click="$router.push(`/users/${user.id}`)">Открыть</button>
     </div> -->
   </div>
+  <div class="page">
+    <div>
+      <p>В проектах, не просмотрено</p>
+      <p v-for="project in userProjects" :key="project.id">
+        {{ project.name }}
+        <span
+          style="display: block"
+          v-for="mr in getMrByProject(project.id)"
+          :key="mr.id"
+        >
+          {{ mr.title }}
+        </span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      userProjects: [],
+      // projMrs: [],
+    };
+  },
+  methods: {
+    getMrByProject(projectId) {
+      return this.noLikeMrs.filter((mr) => mr.projectId === projectId);
+    },
+
+    projNames() {
+      const arrProjectsNames = this.noLikeMrs.flatMap((item2) => {
+        const currentProjectId = this.projects.filter(
+          (item1) => item1["id"] === item2["projectId"]
+        );
+        return currentProjectId;
+      });
+
+      // arrProjectsNames.forEach((project) => {
+      //   project.unlikedMRs = this.getMrByProject(project.id);
+      // });
+
+      return this.userProjects.push(...new Set(arrProjectsNames));
+    },
+  },
   props: {
     user: {
       type: Object,
@@ -25,6 +66,22 @@ export default {
     },
     projects: {
       type: Array,
+    },
+  },
+  mounted() {
+    this.projNames();
+  },
+  computed: {
+    // user() {
+    //   return this.users.find((user) => {
+    //     return user.id === Number(this.user.id);
+    //   });
+    // },
+
+    noLikeMrs() {
+      return this.mergeRequests.filter((mere) => {
+        return !mere.liked.includes(this.user.id);
+      });
     },
   },
 };
@@ -44,5 +101,9 @@ button {
   color: #64766a;
   border: 2px solid #64766a;
   cursor: pointer;
+}
+.page {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
